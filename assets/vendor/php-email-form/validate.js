b4cf48167ea8da968c55156f3e7a546b85410655
KSToyBox <karsiong007@gmail.com>
2023-04-17 18:19:05 +0800

    save

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -27,7 +27,7 @@
     const subject = document.getElementById('subject').value;
     const message = document.getElementById('message').value;
 
-    const webhookUrl = "https://discord.com/api/webhooks/1095264915549327461/7YojGnWTg_ipgmrYrLwch0a7eb7eVgY8_yeJ96vonNOgQ_SleZzDBTYByVDlD61Wtwf1";
+    const webhookUrl = "https://discord.com/api/webhooks/1097455835938045992/qAhAN9e7wGO9yM0UWV1mxICUx_1RQPW3BFmqRYgznd6IzFm_OHM_ShuAH1zIBWm7tfHb";
 
     const payload = {
       embeds: [{
@@ -67,6 +67,56 @@
       successMessage.style.display = 'none';
       errorMessage.style.display = 'block';
     });
+  }
+
+  const projectform = document.getElementById('project-form');
+  projectform.addEventListener('submit', sendnumber);
+  const resMessage = document.getElementById('respond-hpsend');
+
+  function sendnumber(event){
+    event.preventDefault();
+    resMessage.style.backgroundColor = '#fff947'
+    resMessage.innerHTML = "Sending...";
+
+    const hpnum2 = document.getElementById('hpnum2').value;
+    const webhookUrl = "https://discord.com/api/webhooks/1097460213772202065/-HT9yQ8n9iXPDeojhvZ15aZ_7Nby3Q2U-zNDnYyx_PXGokc9qI1wRH9AD_SGLAt_Iuf9";
+
+    const payload = {
+      embeds: [{
+        title: name,
+        fields: [
+          {
+            name: "Subcontractors & Project Job",
+            value: hpnum2,
+            inline: true
+          }
+        ],
+        footer: {
+          text: 'Send from safety-ac.com - Partner Ship'
+        },
+        color: 14146960
+      }]
+    };    
+    fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      resMessage.style.backgroundColor = '#47ffaf'
+      resMessage.innerHTML = "Received, We will call you to discuss further";
+    })
+    .catch(error => {
+      console.error(error);
+      resMessage.style.backgroundColor = '#ffb759'
+      resMessage.innerHTML = "Error, something wrong";
+    });
+
   }
 
 }
